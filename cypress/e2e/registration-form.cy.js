9a769e76d8bfacb99f040c5c1a9bfeb4798dee40
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-06-05 21:59:44 +0300

    update

--- cypress/e2e/registration-form.cy.js
+++ cypress/e2e/registration-form.cy.js
@@ -36,8 +36,8 @@ describe('registration form submit functionality', () => {
     cy.visit('http://localhost:5173')
 
     // Fill the form
-    cy.get("input[name='username']").type("bebras")
-    cy.get("input[name='email']").type("bebras@gmail.com")
+    cy.get("input[name='username']").type("user")
+    cy.get("input[name='email']").type("user@gmail.com")
     cy.get("input[name='password']").type("456123")
     cy.get("input[name='dob']").type("1990-01-01") // Corrected date format
 
@@ -45,38 +45,56 @@ describe('registration form submit functionality', () => {
     cy.contains("button", "Submit").click()
 
     // Check for the specific content
-    cy.get(".submitted-info").should("contain", "Username: bebras")
-    cy.get(".submitted-info").should("contain", "Email: bebras@gmail.com")
+    cy.get(".submitted-info").should("contain", "Username: user")
+    cy.get(".submitted-info").should("contain", "Email: user@gmail.com")
     cy.get(".submitted-info").should("contain", "Date of Birth: 1990-01-01")
     cy.get(".submitted-info").should("contain", "Age: 34")
   })
 })
 
 
-describe('registration form data validation', () => {
+// describe('registration form data validation', () => {
   
-  it('checks validity of Username, Email, Password, and Date of Birth', () => {
-    // Goes to the registration page
-    cy.visit('http://localhost:5173')
+//   it('checks validity of Username, Email, Password, and Date of Birth', () => {
+//     // Goes to the registration page
+//     cy.visit('http://localhost:5173')
 
-    // Username validation: not shorter than 1 symbol
-    cy.get('input[name="username"]').type('a')
-    cy.get('form').submit()
-    cy.get('input[name="username"]').should('have.value', 'a')
+//     // Username validation: not shorter than 1 symbol
+//     cy.get('input[name="username"]').type('a')
+//     cy.get('form').submit()
+//     cy.get('input[name="username"]').should('have.value', 'a')
 
-    // Email validation: should have "@" and "." symbols and symbols before "@", after and after "."
-    cy.get('input[name="email"]').type('test@example.com')
-    cy.get('form').submit()
-    cy.get('input[name="email"]').should('have.value', 'test@example.com')
+//     // Email validation: should have "@" and "." symbols and symbols before "@", after and after "."
+//     cy.get('input[name="email"]').type('test@example.com')
+//     cy.get('form').submit()
+//     cy.get('input[name="email"]').should('have.value', 'test@example.com')
 
-    // Password validation: not shorter than 6 symbols
-    cy.get('input[name="password"]').type('abcdef')
-    cy.get('form').submit()
-    cy.get('input[name="password"]').should('have.value', 'abcdef')
+//     // Password validation: not shorter than 6 symbols
+//     cy.get('input[name="password"]').type('abcdef')
+//     cy.get('form').submit()
+//     cy.get('input[name="password"]').should('have.value', 'abcdef')
 
-    // Date of Birth validation: allow entering year, month, and date
-    cy.get('input[name="dob"]').type('2000-01-01')
-    cy.get('form').submit()
-    cy.get('input[name="dob"]').should('have.value', '2000-01-01') 
+//     // Date of Birth validation: allow entering year, month, and date
+//     cy.get('input[name="dob"]').type('2000-01-01')
+//     cy.get('form').submit()
+//     cy.get('input[name="dob"]').should('have.value', '2000-01-01') 
+//   })
+// })
+
+describe ("Registration form validation", () => {
+  let correctUsername = "user";
+  let correctEmail = "user@gmail.com";
+  let correctPassword = "123456";
+  let correctBirthDate = "2001-01-01";
+
+  it("should display validation errors if submitted empty field", () => {
+    cy.fillFormAndSubmit("", "", "", "");
+
+    cy.get(".error")
+     .should("be.visible")
+      and("contain", "Username is required")
+      and("contain", "Email is required")
+      and("contain", "Password is required")
+      and("contain", "Date of Birth is required")
   })
-})
+});
