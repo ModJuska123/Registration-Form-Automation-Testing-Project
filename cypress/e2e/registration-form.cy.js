a9d2cd3e7759f9caa55caf305a5b4c959909a552
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-06-05 03:07:51 +0300

    update

--- cypress/e2e/registration-form.cy.js
+++ cypress/e2e/registration-form.cy.js
@@ -1,4 +1,4 @@
-describe('registration form functionality', () => {
+describe('registration form display functionality', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:5173')
@@ -31,55 +31,23 @@ describe('registration form functionality', () => {
 
 describe('registration form submit functionality', () => {
   
-  it('Customer is able to fill registration form', () => {
+  it('sees submitted information with username, email, date of birth', () => {
     // Customer goes to page
     cy.visit('http://localhost:5173')
 
     // Fill the form
     cy.get("input[name='username']").type("bebras")
     cy.get("input[name='email']").type("bebras@gmail.com")
-    cy.get("input[name='password']").type("123456")
-    cy.get("input[name='dob']").type("2024-07-05") // Corrected date format
+    cy.get("input[name='password']").type("456123")
+    cy.get("input[name='dob']").type("1990-01-01") // Corrected date format
 
     // Submit the form
-    cy.contains("button", /submit/i).click()
-
-    // Increase timeout for checking the submitted information
-    cy.contains("Submitted Information:", { timeout: 10000 }).should("be.visible")
+    cy.contains("button", "Submit").click()
 
     // Check for the specific content
-    cy.contains("Submitted Information: Username: bebras Email: bebras@gmail.com Date of Birth: 2024-07-05 Age: -1", { timeout: 10000 }).should("be.visible")
+    cy.get(".submitted-info").should("contain", "Username: bebras")
+    cy.get(".submitted-info").should("contain", "Email: bebras@gmail.com")
+    cy.get(".submitted-info").should("contain", "Date of Birth: 1990-01-01")
+    cy.get(".submitted-info").should("contain", "Age: 34")
   })
 })
-
-
-// describe('registration form submit functionality', () => {
-  
-//   it('Customer is able to fill registration form', () => {
-//     // Customer goes to page
-//     cy.visit('http://localhost:5173')
-
-//     // Fill the form
-//     cy.get("input[name='username']").type("bebras")
-//     cy.get("input[name='email']").type("bebras@gmail.com")
-//     cy.get("input[name='password']").type("123456")
-//     cy.get("input[name='dob']").type("2024-07-05") // Corrected date format
-
-//     // Submit the form
-//     cy.contains("button", /submit/i).click()
-
-//     // Verify if the button click worked by checking for a specific change
-//     cy.url().should('include', '/submitted') // Assuming the URL changes after submission
-
-//     // Debug by logging the HTML body
-//     cy.get('body').then(($body) => {
-//       cy.log($body.html()) // Logs the entire HTML of the body
-//       if ($body.find('Submitted Information: Username: bebras Email: bebras@gmail.com Date of Birth: 2024-07-05 Age: -1').length) {
-//         cy.contains("Submitted Information: Username: bebras Email: bebras@gmail.com Date of Birth: 2024-07-05 Age: -1").should("be.visible")
-//       } else {
-//         throw new Error('Submitted information not found')
-//       }
-//     })
-//   })
-// })
-
