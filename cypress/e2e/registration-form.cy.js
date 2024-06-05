4f43e5310569eed8dda8fa3aa5d5c58c88b7fb10
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-06-05 23:41:05 +0300

    update

--- cypress/e2e/registration-form.cy.js
+++ cypress/e2e/registration-form.cy.js
@@ -1,60 +1,57 @@
-describe('registration form display functionality', () => {
-
+describe("registration form display functionality", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5173')
-  })
+    cy.visit("http://localhost:5173");
+  });
 
-  it('displays registration form Username', () => {
-    cy.contains("label", "Username:").should("be.visible")
-    cy.get("input[name='username']").should("be.visible").and("have.value", "")
-  })
+  it("displays registration form Username", () => {
+    cy.contains("label", "Username:").should("be.visible");
+    cy.get("input[name='username']").should("be.visible").and("have.value", "");
+  });
 
-  it('displays registration form Email', () => {
-    cy.contains("label", "Email:").should("be.visible")
-    cy.get("input[name='email']").should("be.visible").and("have.value", "")
-  })
+  it("displays registration form Email", () => {
+    cy.contains("label", "Email:").should("be.visible");
+    cy.get("input[name='email']").should("be.visible").and("have.value", "");
+  });
 
-  it('displays registration form Password', () => {
-    cy.contains("label", "Password:").should("be.visible")
-    cy.get("input[name='email']").should("be.visible").and("have.value", "")
-  })
+  it("displays registration form Password", () => {
+    cy.contains("label", "Password:").should("be.visible");
+    cy.get("input[name='email']").should("be.visible").and("have.value", "");
+  });
 
-  it('displays registration form Date of Birth', () => {
-    cy.contains("label", "Date of Birth:").should("be.visible")
-    cy.get("input[name='dob']").should("be.visible").and("have.value", "")
-  })
+  it("displays registration form Date of Birth", () => {
+    cy.contains("label", "Date of Birth:").should("be.visible");
+    cy.get("input[name='dob']").should("be.visible").and("have.value", "");
+  });
 
-  it('displays Submit Button', () => {
-    cy.contains("button", /submit/i).should("be.visible")
-  })
-})
+  it("displays Submit Button", () => {
+    cy.contains("button", /submit/i).should("be.visible");
+  });
+});
 
-describe('registration form submit functionality', () => {
-  
-  it('sees submitted information with username, email, date of birth', () => {
+describe("registration form submit functionality", () => {
+  it("sees submitted information with username, email, date of birth", () => {
     // Customer goes to page
-    cy.visit('http://localhost:5173')
+    cy.visit("http://localhost:5173");
 
     // Fill the form
-    cy.get("input[name='username']").type("user")
-    cy.get("input[name='email']").type("user@gmail.com")
-    cy.get("input[name='password']").type("456123")
-    cy.get("input[name='dob']").type("1990-01-01") // Corrected date format
+    cy.get("input[name='username']").type("user");
+    cy.get("input[name='email']").type("user@gmail.com");
+    cy.get("input[name='password']").type("456123");
+    cy.get("input[name='dob']").type("1990-01-01"); // Corrected date format
 
     // Submit the form
-    cy.contains("button", "Submit").click()
+    cy.contains("button", "Submit").click();
 
     // Check for the specific content
-    cy.get(".submitted-info").should("contain", "Username: user")
-    cy.get(".submitted-info").should("contain", "Email: user@gmail.com")
-    cy.get(".submitted-info").should("contain", "Date of Birth: 1990-01-01")
-    cy.get(".submitted-info").should("contain", "Age: 34")
-  })
-})
-
+    cy.get(".submitted-info").should("contain", "Username: user");
+    cy.get(".submitted-info").should("contain", "Email: user@gmail.com");
+    cy.get(".submitted-info").should("contain", "Date of Birth: 1990-01-01");
+    cy.get(".submitted-info").should("contain", "Age: 34");
+  });
+});
 
 // describe('registration form data validation', () => {
-  
+
 //   it('checks validity of Username, Email, Password, and Date of Birth', () => {
 //     // Goes to the registration page
 //     cy.visit('http://localhost:5173')
@@ -77,11 +74,11 @@ describe('registration form submit functionality', () => {
 //     // Date of Birth validation: allow entering year, month, and date
 //     cy.get('input[name="dob"]').type('2000-01-01')
 //     cy.get('form').submit()
-//     cy.get('input[name="dob"]').should('have.value', '2000-01-01') 
+//     cy.get('input[name="dob"]').should('have.value', '2000-01-01')
 //   })
 // })
 
-describe ("Registration form validation", () => {
+describe("Registration form validation", () => {
   let correctUsername = "user";
   let correctEmail = "user@gmail.com";
   let correctPassword = "123456";
@@ -90,11 +87,10 @@ describe ("Registration form validation", () => {
   it("should display validation errors if submitted empty field", () => {
     cy.fillFormAndSubmit("", "", "", "");
 
-    cy.get(".error")
-     .should("be.visible")
-      and("contain", "Username is required")
-      and("contain", "Email is required")
-      and("contain", "Password is required")
-      and("contain", "Date of Birth is required");
-  })
+    cy.get(".error").should("be.visible")
+    .and("contain", "Username is required")
+    .and("contain", "Email is required")
+    .and("contain", "Password is required")
+    .and("contain", "Date of Birth is required");
+  });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("fillFormAndSubmit", (username = '', email = '', password = '', dateOfBirth = '') => {
+    cy.visit('http://localhost:5173/')
+    if (username) {
+        cy.get('[data-cy="username"]').type(username)
+    }
+    if (email) {
+        cy.get('[data-cy="email"]').type(email)
+    }
+    if (password) {
+        cy.get('[data-cy="password"]').type(password)
+    }
+    if (dateOfBirth) {
+        cy.get('[data-cy="date-of-birth"]').type(birthDate)
+    }
+    cy.get('[data-cy="submit"]').click()
+  });
+  
